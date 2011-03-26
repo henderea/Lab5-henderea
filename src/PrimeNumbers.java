05637cba25e5e132857147da6e51f865bbbdef10
Eric Henderson <henderea@rose-hulman.edu>
2011-03-26 16:15:05 -0400

    Passed testFour

--- src/PrimeNumbers.java
+++ src/PrimeNumbers.java
@@ -6,6 +6,11 @@ public class PrimeNumbers {
 	public static ArrayList<Integer> generatePrimes(int n)
 	{
 		ArrayList<Integer> rval = new ArrayList<Integer>();
+		if(n % 2 == 0)
+		{
+			rval.add(2);
+			n /= 2;
+		}
 		if(n > 1)
 		{
 			rval.add(n);
