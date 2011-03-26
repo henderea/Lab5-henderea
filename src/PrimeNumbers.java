8ce31df63048e90a16072e53dd43067398150b34
Eric Henderson <henderea@rose-hulman.edu>
2011-03-26 16:08:25 -0400

    Passed testTwo

--- src/PrimeNumbers.java
+++ src/PrimeNumbers.java
@@ -3,8 +3,13 @@ import java.util.ArrayList;
 
 public class PrimeNumbers {
 
-	public static ArrayList<Integer> generatePrimes(int n) {
-		
-		return new ArrayList<Integer>();
+	public static ArrayList<Integer> generatePrimes(int n)
+	{
+		ArrayList<Integer> rval = new ArrayList<Integer>();
+		if(n > 1)
+		{
+			rval.add(2);
+		}
+		return rval;
 	}
 }
