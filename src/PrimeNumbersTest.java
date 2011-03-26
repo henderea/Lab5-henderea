b0b27ac3d6a95579daea3c6284d49e7961856977
Eric Henderson <henderea@rose-hulman.edu>
2011-03-26 15:55:51 -0400

    created testOne and list methods in PrimeNumbersTest.java

--- src/PrimeNumbersTest.java
+++ src/PrimeNumbersTest.java
@@ -1,5 +1,26 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
 
 
-public class PrimeNumbersTest {
 
+public class PrimeNumbersTest
+{
+	@Test
+	public void testOne()
+	{
+		assertEquals(new ArrayList<Integer>(), PrimeNumbers.generatePrimes(1));
+	}
+	
+	public ArrayList<Integer> list(int... ints)
+	{
+		ArrayList<Integer> items = new ArrayList<Integer>();
+		for(int i : ints)
+		{
+			items.add(i);
+		}
+		return items;
+	}
 }
